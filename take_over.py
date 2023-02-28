5bf86d0648d16e5f04ff022cd4858c2da3338593
Wigglesworth <b1h13189@htlf.com>
2023-02-28 11:37:58 -0600

    fear the dark web

---
+++ take_over.py
@@ -0,0 +1,4 @@
+# Hello Infomation Security. Your security scan worked.
+with open("the_dark_web_has_taken_over.txt", "w") as file:
+    for i in range(1000):
+        file.write("I have taken over this text file - fear the Dark Web.\n")
