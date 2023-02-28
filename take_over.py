67ed745d64c4e6d10f94787c8832ec31bf6401ca
Matthew Wigglesworth <62004884+Matt-Wigg@users.noreply.github.com>
2023-02-28 11:39:47 -0600

    the dark web makes no mistakes

--- take_over.py
+++ take_over.py
@@ -1,4 +1,4 @@
-# Hello Infomation Security. Your security scan worked.
+# Hello Information Security. Your security scan worked.
 with open("the_dark_web_has_taken_over.txt", "w") as file:
     for i in range(1000):
         file.write("I have taken over this text file - fear the Dark Web.\n")
